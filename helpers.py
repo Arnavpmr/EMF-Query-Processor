1941b5276626179a116738dc80049c76547bf4c5
Arnavpmr <arnavmar16@gmail.com>
2024-11-22 20:53:06 -0500

    Fixed bug where empty lines in stdin are interpreted as an empty string instead of any empty list

--- helpers.py
+++ helpers.py
@@ -1,5 +1,7 @@
 # Every input that is a list is in the form of item1; item2; item3;...
 def parse_list_input(input: str) -> list[str]:
+    if input.strip() == "": return []
+
     return input.strip().replace("\n", "").split(";")
 
 # Returns a string with spaces for the given number of tabs
